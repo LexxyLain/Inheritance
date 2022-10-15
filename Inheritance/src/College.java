dc572e8f8ed95977b7bf0f9ca9f86b74df727616
LexxyLain <100071781+LexxyLain@users.noreply.github.com>
2022-10-15 20:41:42 +0800

    Add files via upload

---
+++ Inheritance/src/College.java
@@ -0,0 +1,9 @@
+package com.Inheritance.sample;
+class College{
+    public void Classcode(){
+        System.out.println("ITP203");
+    }
+    public void CourseSubject() {
+        System.out.println("Object Oriented Programming");
+    }
+}
